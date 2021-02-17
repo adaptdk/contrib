32632394df27ca8345cc4c7ba30534820d68a6eb
Marco Villegas <marco.villegas@adaptagency.com>
2021-02-16 22:00:47 -0500

    Support overriding templates directory
    And use the vendor directory by default.

--- src/Console/Command/GenerateCommand.php
+++ src/Console/Command/GenerateCommand.php
@@ -74,9 +74,22 @@ class GenerateCommand extends Command {
      *   Name of the twig template to use.
      * @param array $variables
      *   Variables to pass to twig render.
+     *
+     * @throws \Exception
+     *   Cannot find the template.
      */
     protected function twigRender(OutputInterface $output, $template_name, $variables) {
-        $loader = new FilesystemLoader('templates');
+        $candidate_template_files = ["templates/$template_name", "vendor/adaptdk/contrib/templates/$template_name"];
+        foreach ($candidate_template_files as $template_file) {
+            if (file_exists($template_file)) {
+                $directory = dirname($template_file);
+                break;
+            }
+        }
+        if (empty($directory)) {
+            throw \Exception('Cannot find t valid "templates" directory with the needed template.');
+        }
+        $loader = new FilesystemLoader($directory);
         $twig = new Environment($loader, [
             'cache' => 'cache',
         ]);
