e1756b675d1e32ed19759f47c7506c51db8711ef
Marco Villegas <marco.villegas@adaptagency.com>
2023-11-22 18:19:46 -0500

    Make lint command compatible with symfony 6.3

--- src/Console/Command/LintCommand.php
+++ src/Console/Command/LintCommand.php
@@ -2,6 +2,7 @@
 
 namespace ContribLog\Console\Command;
 
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Yaml\Command\LintCommand as LintBaseCommand;
@@ -9,9 +10,8 @@ use Symfony\Component\Yaml\Command\LintCommand as LintBaseCommand;
 /**
  * Lint command.
  */
+#[AsCommand(name: 'lint', description: 'Lint a YAML file and outputs encountered errors')]
 class LintCommand extends LintBaseCommand {
-
-    protected static $defaultName = 'lint';
 
     /**
      * {@inheritdoc}
@@ -19,15 +19,15 @@ class LintCommand extends LintBaseCommand {
     protected function configure()
     {
         $this
-            ->setDescription('Lints a file and outputs encountered errors')
             ->addArgument('filename', InputArgument::IS_ARRAY | InputArgument::OPTIONAL, 'A file, a directory or "-" for reading from STDIN', ['contributions.yml'])
-            ->addOption('format', null, InputOption::VALUE_REQUIRED, 'The output format', 'txt')
-            ->addOption('parse-tags', null, InputOption::VALUE_NONE, 'Parse custom tags')
+            ->addOption('format', null, InputOption::VALUE_REQUIRED, 'The output format ("txt, json, github")')
+            ->addOption('exclude', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Path(s) to exclude')
+            ->addOption('parse-tags', null, InputOption::VALUE_NEGATABLE, 'Parse custom tags', null)
             ->setHelp(<<<EOF
 The <info>%command.name%</info> command lints a YAML file and outputs to STDOUT
 the first encountered syntax error.
 
-You can validate YAML contents passed from STDIN:
+You can validates YAML contents passed from STDIN:
 
   <info>cat filename | php %command.full_name% -</info>
 
@@ -39,6 +39,10 @@ Or of a whole directory:
 
   <info>php %command.full_name% dirname</info>
   <info>php %command.full_name% dirname --format=json</info>
+
+You can also exclude one or more specific files:
+
+  <info>php %command.full_name% dirname --exclude="dirname/foo.yaml" --exclude="dirname/bar.yaml"</info>
 
 EOF
             )
