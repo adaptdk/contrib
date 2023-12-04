ca7232dae0af837bbec5fcad4c30a834e1125ca5
Marco Villegas <marco.villegas@adaptagency.com>
2023-12-03 23:15:27 -0500

    Use laminas-feed to generate RSS

--- src/Console/Command/RssCommand.php
+++ src/Console/Command/RssCommand.php
@@ -2,6 +2,7 @@
 
 namespace ContribLog\Console\Command;
 
+use Laminas\Feed\Writer\Feed;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -58,6 +59,59 @@ class RssCommand extends FeedCommandBase {
      * {@inheritdoc}
      */
     protected function generateFeed(OutputInterface $output): void {
-        $this->twigRender($output, 'contributions.rss.twig', $this->prepareVariables());
+        $variables = $this->prepareVariables();
+        $authors = $variables['people'];
+        $projects = $variables['projects'];
+        $organization = $variables['organization'];
+        $contribution_types = $variables['types'];
+
+        $feed = new Feed;
+        $feed->setTitle(sprintf("%s's Open Source Contributions", $this->get('organization.name', $variables)));
+        $feed->setLink($this->get('config.html.url', $variables));
+        $feed->setDescription($this->get('organization.introduction', $variables));
+        $feed->setFeedLink($this->get('config.rss.url', $variables), 'rss');
+        $feed->setDateModified(time());
+        $feed->setLanguage($this->get('config.language', $variables));
+
+        foreach ($variables['contributions'] as $contribution) {
+            $entry = $feed->createEntry();
+            $entry->setTitle($contribution['title']);
+            $entry->setDateCreated($contribution['start']);
+            $entry->setDateModified($contribution['start']);
+            $tag = sprintf('tag:%s,%s:%s', $organization['domain'], $contribution['start']->format('Y-m-d'), \urlencode($contribution['title']));
+            $entry->setId($tag);
+            $entry->addAuthor([
+                'name'  => $authors[$contribution['who']],
+            ]);
+            $description = sprintf('<p>By <em>%s</em> for <a href="%s">%s</a>.</p>
+            <p>%s</p>',
+              $authors[$contribution['who']],
+              $projects[$contribution['project']]['url'],
+              $projects[$contribution['project']]['name'],
+              $contribution['description']
+            );
+            if (!empty($contribution['links'])) {
+                $links = '';
+                foreach ($contribution['links'] as $link) {
+                    $links = sprintf('<li><a href="%s">%s</a></li>', $link, $link);
+                }
+                $description .= sprintf('<ul>%s</ul>', $links);
+            }
+            $description = sprintf('<article>%s</article>', $description);
+            $entry->setDescription($description);
+            $entry->setContent($description);
+            $entry->addCategory([
+                'term' => $contribution_types[$contribution['type']],
+            ]);
+            foreach ($projects[$contribution['project']]['tags'] as $tag) {
+                $entry->addCategory([
+                    'term' => $tag,
+                ]);
+            }
+            $feed->addEntry($entry);
+        }
+
+        $output->write($feed->export('rss'));
     }
+
 }
